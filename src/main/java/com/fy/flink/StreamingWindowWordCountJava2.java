922001179eee4a78fd9fa3274a55ed0f2fee18c7
fuyu <116492151@qq.com>
2018-05-21 15:36:05 +0800

    eclipse最后一次提交代码

--- src/main/java/com/fy/flink/StreamingWindowWordCountJava2.java
+++ src/main/java/com/fy/flink/StreamingWindowWordCountJava2.java
@@ -63,6 +63,8 @@ public class StreamingWindowWordCountJava2 {
     public static void eclipseAdd() {
     	System.out.println("eclipse add method");
     	System.out.println("eclipse add method");
+    	System.out.println("eclipse add method");
+    	System.out.println("eclipse add method");
     }
     
     /**
